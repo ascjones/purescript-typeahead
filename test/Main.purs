a4e34d52ff861f1c312786d5f61c1cdb13049630
Andrew Jones <ascjones@gmail.com>
2015-10-12 23:04:09 +0100

    Make USA states example work

--- test/Main.purs
+++ test/Main.purs
@@ -7,14 +7,31 @@ import Typeahead
 import Control.Monad.Eff
 import Control.Monad.Eff.Console
 
+import Data.Array
 import Data.Function
+import Data.String.Regex
 
 import qualified Control.Monad.Eff.JQuery as J
 
+states :: Array String
+states =
+  [ "Alabama", "Alaska", "Arizona", "Arkansas", "California"
+  , "Colorado", "Connecticut", "Delaware", "Florida", "Georgia", "Hawaii"
+  , "Idaho", "Illinois", "Indiana", "Iowa", "Kansas", "Kentucky", "Louisiana"
+  , "Maine", "Maryland", "Massachusetts", "Michigan", "Minnesota"
+  , "Mississippi", "Missouri", "Montana", "Nebraska", "Nevada", "New Hampshire"
+  , "New Jersey", "New Mexico", "New York", "North Carolina", "North Dakota"
+  , "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Rhode Island"
+  , "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Vermont"
+  , "Virginia", "Washington", "West Virginia", "Wisconsin", "Wyoming" ]
+
 main = do
-  states <- J.select "#main .typeahead"
-  typeahead states defaultOptions [{ name : "states", source : substringMatcher }]
+  statesInput <- J.select "#main .typeahead"
+  let source = substringMatcher states
+  typeahead statesInput defaultOptions [{ name : "states", source : source }]
 
   where
-  substringMatcher :: Source
-  substringMatcher = mkFn2 $ \q cb -> do cb ["Michigan", "California"]
+  substringMatcher :: Array String -> Source
+  substringMatcher strs = mkFn2 $ \q cb -> do
+    let substrRegex = regex q (parseFlags "i")
+    cb $ filter (test substrRegex) strs
